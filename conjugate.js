6b70ce2c66393a7ba071739fb902d0a5688e45a7
juanchel <juanchen.li@redbeacon.com>
2015-07-27 17:19:50 -0700

    simple past conjugation

--- conjugate.js
+++ conjugate.js
@@ -1,6 +1,15 @@
 'use strict';
 
-var correct = 'にほん'
+var correct = 'たべる';
+
+$(document).ready(function() {
+    $('textarea').bind('keypress', function(e) {
+        if ((e.keyCode || e.which) == 13) {
+            $(this).parents('form').submit();
+            return false;
+        }
+    }); 
+});
 
 function submitAnswer() {
     if ($('#answer').val() == correct) {
@@ -9,5 +18,16 @@ function submitAnswer() {
 }
 
 function nextQuestion() {
+    var word = ichidan[Math.floor(Math.random() * ichidan.length)];
+    correct = conjugate(word, 0);
+    console.log(correct);
+    $('#question-word').text(word);
     $('#answer').val('');
 }
+
+function conjugate(word, mods) {
+    var res = word.substring(0, word.length - 1) + 'た';
+    return res;
+}
+
+var ichidan = ['たべる', 'ねる', 'しんじる', 'ねる', 'おきる', 'きる', 'でる', 'かける'];
